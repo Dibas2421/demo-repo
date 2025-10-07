e87d7d25650244ee1279e8285dab8cbb31c033bd
Dibasd <Dibasd@mindfiresolutions.com>
2025-10-07 21:46:47 +0530

    updated test_1 and test_7 files

--- test_1.py
+++ test_1.py
@@ -1,3 +1,4 @@
 print("Hello World")
 print("This is test file 1")
 print("This is my first file")
+print("This is to check whether webhooks are working or not")
--- test_7.py
+++ test_7.py
@@ -1,2 +1,3 @@
 print("This is test file 7")
 print("It's been 9 months since I last edited this file")
+print("This is my seventh file")
